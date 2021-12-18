143161caefe62740ac39504166abd7c6ce5eb1d2
Jordan Thomas <jkt856@gmail.com>
2021-12-18 10:17:54 -0500

    adding deliverable 2 and 3 to Rscript.

--- MechaCarChallenge.Rscript.R
+++ MechaCarChallenge.Rscript.R
@@ -11,3 +11,24 @@ lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AW
 
 # Generate summary statistics
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data =MechaCar_table))
+
+# Deliverable 2
+
+# Import and read CSV as DF
+Suspension_coil_table <- read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFactors = F)
+
+# Create a summary DF
+total_summary <- Suspension_coil_table %>% summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI))
+
+# Create a lot summary DF using groupby
+lot_summary <- Suspension_coil_table %>% group_by(Manufacturing_Lot) %>% summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI), .groups = 'keep')
+
+# Deliverable 3
+
+# T-Test on suspension coils
+t.test(Suspension_coil_table$PSI, mu=1500)
+
+# T-Test on suspension coils per lot
+t.test(subset(Suspension_coil_table,Manufacturing_Lot=="Lot1")$PSI, mu = 1500)
+t.test(subset(Suspension_coil_table,Manufacturing_Lot=="Lot2")$PSI, mu = 1500)
+t.test(subset(Suspension_coil_table,Manufacturing_Lot=="Lot3")$PSI, mu = 1500)
